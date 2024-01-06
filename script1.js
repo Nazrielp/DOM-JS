fbe47c640c9873be084af584968a312c6252e39e
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-01-06 12:43:29 +0700

    getElement

--- script1.js
+++ script1.js
@@ -8,15 +8,18 @@ judul.style.backgroundColor = '#ccc';
 
 //document.getElementsByTagName() -> HTMLCollection
 const p = document.getElementsByTagName('p');
-p[0].style.backgroundColor = 'red';
+p[0].style.backgroundColor = 'lightblue';
 p[1].style.backgroundColor = 'lightblue';
+p[2].style.backgroundColor = 'lightblue';
+p[3].style.backgroundColor = 'lightblue';
+
 
 const h1 = document.getElementsByTagName('h1')[0];
 h1.style.fontSize = '50px';
 
 // document.getElementsByClassName()  -> HTMLCollection
 const p1 = document.getElementsByClassName('p1')[0];
-p1.innerHTML = 'ini di ubah di Java Script'
+p1.innerHTML = 'Paragraf 1 js'
 p1.style.fontSize = '30px'
 
 
