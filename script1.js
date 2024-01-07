d85117524409ccbf89df57c79dd832f09cc7e527
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-01-07 10:50:30 +0700

    getElement and querySelector

--- script1.js
+++ script1.js
@@ -1,9 +1,8 @@
 //doocument.getElementById() -> element
 const judul = document.getElementById('judul') ;
-judul.style.color = 'White';
 judul.style.backgroundColor = '#ccc';
 //untuk mengganti h1 tanpa mengubahnya di html
-// judul.innerHTML = 'Zriel'
+// judul.innerHTML = 'Zriel';
 
 
 //document.getElementsByTagName() -> HTMLCollection
@@ -19,7 +18,17 @@ h1.style.fontSize = '50px';
 
 // document.getElementsByClassName()  -> HTMLCollection
 const p1 = document.getElementsByClassName('p1')[0];
-p1.innerHTML = 'Paragraf 1 js'
-p1.style.fontSize = '30px'
+p1.innerHTML = 'Paragraf 1 js';
+p1.style.fontSize = '30px';
 
+// menberikan style di Java script
+// document.querySelector() -> Element
+const p4 = document.querySelector('#b p');
+p4.style.color = 'red';
 
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+// document.querySelectorAll() -> nodelist
+const p2 = document.querySelectorAll('section#a p');
+p2[2].style.color = 'yellow';
