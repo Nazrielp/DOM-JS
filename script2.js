c79b3bb7f22afaa281488cd994e5904f87d79f54
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-01-17 19:53:22 +0700

    getElement and querySelector

---
+++ script2.js
@@ -0,0 +1,16 @@
+const judul = document.getElementById ('judul');
+judul.innerHTML = '<em>Zriel</em>';
+
+const sectionA = document.querySelector ('section#a')
+sectionA.innerHTML = 'Zriel ganteng'
+
+
+
+// element.style.<properti css>
+const h1 = document.querySelector('#judul');
+h1.style.backgroundColor = 'black';
+h1.style.color = 'red';
+
+const h = document.getElementsByTagName('h1')[0];
+h.setAttribute('name', 'zriel');
+    
